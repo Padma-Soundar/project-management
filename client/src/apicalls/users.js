f0a8f5ebe0a4ef9c5b6cee6402a20d4cc10a96c6
Padma-Soundar <s.padmavathy1995@gmail.com>
2024-11-25 18:03:02 +0530

    Update users.js

--- client/src/apicalls/users.js
+++ client/src/apicalls/users.js
@@ -1,5 +1,5 @@
 const { apiRequest } = require(".");
 
-export const RegisterUser = async (payload) => apiRequest('post', '/api/users/register', payload);
+export const RegisterUser = async (payload) => apiRequest('post', 'https://project-management-d5e2.onrender.com/api/users/register', payload);
 export const LoginUser = async (payload) => apiRequest('post', 'https://project-management-d5e2.onrender.com/api/users/login', payload);
-export const GetLoggedInUser = async () => apiRequest('get', '/api/users/get-logged-in-user');
+export const GetLoggedInUser = async () => apiRequest('get', 'https://project-management-d5e2.onrender.com/api/users/get-logged-in-user');
