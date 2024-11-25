4d90dc0a53e62400bb4adc55a0f6eec663d46f11
Padma-Soundar <s.padmavathy1995@gmail.com>
2024-11-25 18:06:02 +0530

    Update tasks.js

--- client/src/apicalls/tasks.js
+++ client/src/apicalls/tasks.js
@@ -1,17 +1,17 @@
 import { apiRequest } from ".";
 
 export const CreateTask = async (task) =>
-  apiRequest("post", "/api/tasks/create-task", task);
+  apiRequest("post", "https://project-management-d5e2.onrender.com/api/tasks/create-task", task);
 
 export const GetAllTasks = async (filters) =>
-  apiRequest("post", "/api/tasks/get-all-tasks", filters);
+  apiRequest("post", "https://project-management-d5e2.onrender.com/api/tasks/get-all-tasks", filters);
 
 export const UpdateTask = async (task) =>
-  apiRequest("post", "/api/tasks/update-task", task);
+  apiRequest("post", "https://project-management-d5e2.onrender.com/api/tasks/update-task", task);
 
 export const DeleteTask = async (id) =>
-  apiRequest("post", "/api/tasks/delete-task", { _id: id });
+  apiRequest("post", "https://project-management-d5e2.onrender.com/api/tasks/delete-task", { _id: id });
 
 export const UploadImage = async (payload) => {
-  return apiRequest("post", "/api/tasks/upload-image", payload);
+  return apiRequest("post", "https://project-management-d5e2.onrender.com/api/tasks/upload-image", payload);
 };
